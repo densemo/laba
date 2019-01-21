41cbf651e3adaad0d3929cd577ee9cbbbf48c8bd
jon <dfi@mail.ru>
2019-01-21 14:30:57 +0300

    java

---
+++ popp.java
@@ -0,0 +1,25 @@
+public class popp {
+    double asd;
+
+  double zxc;
+
+    popp(double q, double a) {
+        asd = q;
+        zxc = a;
+
+
+        System.out.println(asd - zxc);
+    }
+}
+    class poppnex {
+        public static void main(String args[]){
+            popp pop1 = new popp(1.10,1.00);
+           // popp pop2 = new popp(-1,-1);
+         //   popp pop3 = new popp(20,30);
+
+
+           // System.out.println(asd-zxc);
+          //  System.out.println(pop1.equals(pop3));
+        }
+    }
+
